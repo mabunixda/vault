615110912e27644cb3e7dadb90117b8971049292
Edmund Haselwanter <ehaselwanter@infralovers.com>
2021-04-29 19:46:49 +0200

    add terraform example

--- setup.sh
+++ setup.sh
@@ -1,25 +1,41 @@
 #!/usr/bin/env bash
 
 VAULT_VERSION=${VAULT_VERSION:-1.7.1}
+VAULT_ENTERPRISE=${VAULT_ENTERPRISE:-+ent}
 VAULT_PORT=8200
 
 INSTALLED=$(vault --version | awk '{print $2}' || echo "")
 
-if [ -z "$INSTALLED" ] || [ "$INSTALLED" != "v${VAULT_VERSION}" ]; then
+if [ -z "$INSTALLED" ] || [ "$INSTALLED" != "v${VAULT_VERSION}${VAULT_ENTERPRISE}" ]; then
 
+    set -ex
     mkdir -p ${HOME}/bin/
-    curl -o /tmp/vault_${VAULT_VERSION}_linux_amd64.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+    curl -o /tmp/vault_${VAULT_VERSION}_linux_amd64.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}${VAULT_ENTERPRISE}/vault_${VAULT_VERSION}${VAULT_ENTERPRISE}_linux_amd64.zip
     unzip -d ${HOME}/bin/ -o /tmp/vault_${VAULT_VERSION}_linux_amd64.zip
+    sudo cp ${HOME}/bin/vault /usr/local/bin/
     rm -f /tmp/vault_${VAULT_VERSION}_linux_amd64.zip
 
 fi
 
 
+TERRAFORM_VERSION=${TERRAFORM_VERSION:-0.15.1}
+TF_INSTALLED=$(terraform --version| grep Terraform | awk '{print $2}' || echo "")
+
+if [ -z "$TF_INSTALLED" ] || [ "$TF_INSTALLED" != "v${TERRAFORM_VERSION}" ]; then
+
+    mkdir -p ${HOME}/bin/
+    curl -o /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    unzip -d ${HOME}/bin/ -o /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    rm -f /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+fi
+
 DOCKER_STATE=$(docker inspect -f '{{.State.Running}}'  vault || echo 'false')
 if [ $DOCKER_STATE != "true" ]; then
     docker rm -f vault
-
-    docker pull -q vault:${VAULT_VERSION}
+    
+    docker pull hashicorp/vault-enterprise:${VAULT_VERSION}_ent
+    #docker pull -q vault:${VAULT_VERSION}
 
     docker run \
         -d \
@@ -28,7 +44,7 @@ if [ $DOCKER_STATE != "true" ]; then
         --cap-add=IPC_LOCK \
         -e "VAULT_DEV_ROOT_TOKEN_ID=myroot" \
         -e "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:${VAULT_PORT}" \
-        vault:${VAULT_VERSION}
+        hashicorp/vault-enterprise:${VAULT_VERSION}_ent
 
 fi
 
@@ -36,3 +52,10 @@ echo "export VAULT_ADDR=\"http://127.0.0.1:${VAULT_PORT}\"" > ${HOME}/.extra.vau
 echo "export VAULT_TOKEN=myroot" >> ${HOME}/.extra.vault
 
 source ${HOME}/.extra.vault
+
+echo "=============="
+terraform --version
+echo "=============="
+vault --version
+echo "=============="
+vault status
